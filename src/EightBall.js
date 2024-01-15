9affafc469c9ba6c95807fd40d117c36f825b98b
mrtomno <adel.ngo@yahoo.com>
2024-01-15 14:41:34 -0600

    format spacing

--- src/EightBall.js
+++ src/EightBall.js
@@ -11,7 +11,7 @@ const EightBall = ({answers}) => {
     }
     const [answer, setAnswer] = useState({msg: "Think of a Question", color: 'black'});
     return(
-        <div className="EightBall-container" style={{backgroundColor: answer.color}}onClick={makeAnswer}>
+        <div className="EightBall-container" style={{backgroundColor: answer.color}} onClick={makeAnswer}>
             <h2 className="Answer">{answer.msg}</h2>
         </div>
     )
