4dee560b366c14aab61be1c8d20ba0052729c961
mrtomno <adel.ngo@yahoo.com>
2024-01-15 14:19:55 -0600

    add intial state

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,9 +1,20 @@
 import React , {useState} from 'react';
 import "./EightBall.css";
 
-const EightBall = (props) => {
+const EightBall = ({answers}) => {
+    // Generate a random index for answer
+    const genRandom = () => Math.floor(Math.random() * answers.length) 
+    const makeAnswer = () => {
+        let randIdx = genRandom();
+        let newAnswer = {msg: answers[randIdx].msg, color: answers[randIdx].color}
+        setAnswer(newAnswer);
+    }
+    const [answer, setAnswer] = useState({msg: "Think of a Question", color: 'black'});
     return(
-        <h1>EightBall goes here</h1>
+        <div className="EightBall" onClick={makeAnswer}>
+            <p1>{answer.msg}</p1>
+            <p1>{answer.color}</p1>
+        </div>
     )
 }
 
