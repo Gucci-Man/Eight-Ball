0c5e2b1b1339f873f6d353110a4d9ab544b4139b
mrtomno <adel.ngo@yahoo.com>
2024-01-15 11:33:39 -0600

    set up files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import EightBall from './EightBall';
+import './App.css';
+
+function App() {
+  return (
+    <EightBall />
+  );
+}
+
+export default App;
---
+++ src/EightBall.js
@@ -0,0 +1,10 @@
+import React , {useState} from 'react';
+import "./EightBall.css";
+
+const EightBall = (props) => {
+    return(
+        <h1>EightBall goes here</h1>
+    )
+}
+
+export default EightBall;
